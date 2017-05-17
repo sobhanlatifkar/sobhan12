7ceaf305d8f92251b9c7d5af2937aa309362d437
sobhanlatifkar <sobhan.latifkar@gmail.com>
2017-05-18 01:14:51 +0430

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,4 +1,4 @@
-bot_token = ""
+bot_token = "391929482:AAHebm1EqRY32H8X3SF_UNdtjhGzWlcRvuk"
 send_api = "https://api.telegram.org/bot"..bot_token
 bot_version = "2.5"
-sudo_id = 212400472
+sudo_id = 247789358
